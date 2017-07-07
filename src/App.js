45625cefa7031916f9e23da283b1dc1d5415da60
Romina80008 <romina_jaramillo@hotmail.com>
2017-07-06 21:08:44 -0500

    Laboratorio  3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import SearchBox from './components/SearchBox';
+import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
+
+const API_URL = 'https://address-book-api-kfpkaqtghu.now.sh';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state={
-        searchText: 'Busqueda',
-        firstName: ''; 
-
+        contacts: [], 
+        searchText: '',
+        firstName: '', 
+        lastName: '',
+        phone: ''
     };
   }
 
+
+  componentDidMount(){
+    this.getContacts();
+
+  }
+
+  getContacts = () => {
+    axios({
+    method: 'GET',
+    url: API_URL + '/api/contacts',
+    headers: {
+    'Api-Key':'1719069385',
+    }
+    })
+    .then((response)=>{
+    this.setState({
+    contacts: response.data.data
+    });
+    console.log(this.state.contacts);
+    })
+    .catch((error)=>{
+    console.log(error);
+    })
+    }
+
+  saveContact = (contact) => {
+    axios({
+      method: 'POST',
+      url: API_URL + '/api/contacts',
+      headers:{
+        'Api-Key': '1719069385',
+        'Content-Type': 'application/json',
+
+      },
+      data:{
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        phone: contact.phone,
+      },
+    }).then((response)=>{
+      console.log(response);
+      this.getContacts();
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   handleSearchTextChange = (event) => {
-      this.setState({
+      this.setState({ 
         searchText: event.target.value
+      });
+
+  }
+
+    handleFirstNameChange = (event) => {
+      this.setState({
+        firstName: event.target.value
+      });
+
+  }
+
+    handleLastNameChange = (event) => {
+      this.setState({
+        lastName: event.target.value
+      });
+
+  }
+
+    handlePhoneChange = (event) => {
+      this.setState({
+        phone: event.target.value
       });
 
   }
@@ -25,36 +101,48 @@ class App extends Component {
 
 
   render() {
+    const contacts = this.state.contacts.filter((contact,index) =>{
+      if(contact.firstName.indexOf(this.state.searchText) > -1){
+
+        return true; 
+      }
+      return false; 
+    });
+
     return (
-      <div> 
-      <Header title="Gabriel"/>
-      <div></div>
-      <SearchBox 
-        value={this.state.searchText}
-        onChange={this.handleSearchTextChange}
-      />
-      Hola mundo Paula
+<div>
+<Header title="Address Book"/>
+<div className="container">
+<div className="row">
+<div className="col-md-6">
+<SearchBox 
+value={this.state.searchText}
+onChange={this.handleSearchTextChange}/>
+<ContactList contacts={contacts}/>
+</div>
+<div className="col-md-6">
+<h1>Nuevo contacto</h1>
+<ContactForm 
+firstName={this.state.firstName}
+lastName={this.state.lastName}
+phone={this.state.phone}
+handleFirstNameChange={this.handleFirstNameChange}
+handleLastNameChange={this.handleLastNameChange}
+handlePhoneChange={this.handlePhoneChange}
+saveContact={this.saveContact}
+/>
+</div>
+</div>
+</div>
+Hola Mundo
+<Footer title="Copyright 2017 - PUCE"/>
+</div>
 
-      <div className="row">
-      <div className="col-md-6">
-      <h1>Nuevo Contacto </h1>
-      </div>
 
-      </div>
-
-
-
-      <Footer copy="Pie de pagina!"/>
- 
-
-       </div>
 
     );
   }
 }
-
-//const obj = {a: 1, b: 's', c: true};
-
 
 
 export default App;
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,21 +5,34 @@ const ContactForm = (props) => (
 		<input 
 			type="text" 
 			value={props.firstName}
-			onChange={props.onChange} 
+			onChange={props.handleFirstNameChange} 
 		/>
 		<input 
 			type="text" 
 			value={props.lastName}
-			onChange={props.onChange} 
+			onChange={props.handleLastNameChange} 
 		/>
 		<input 
 			type="text" 
 			value={props.phone}
-			onChange={props.onChange} 
+			onChange={props.handlePhoneChange} 
 		/>
+		<div className="text-center">
+		<button
+			className="btn btn-primary"
+			onClick={() => props.saveContact({
+				firstName: props.firstName,
+				lastName: props.lastName,
+				phone: props.phone
+
+			})}
+		/>
+
+		</div>
 
 
 	</div>
+
 
 );
 
